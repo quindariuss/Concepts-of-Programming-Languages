9ddb945f4b66a25cf4c88c82d8d38e2bffaaebff
Quin'darius Ali Lyles-Woods <quinwoodz@gmail.com>
2021-09-27 22:44:27 -0400

    Committing project files

---
+++ Project/First Deliverable/Code/lexer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define IN 1 
+#define OUT 0 
+
+int main()
+{
+	int character, 				// Gets individual character input
+	    tokens, 				// Count of tokens 
+	    state;				// Determines if we are inside or outside a word
+	state = OUT;
+	tokens = 0; 
+	while ((character = getchar()) != EOF)	//Loops through the input		
+	{
+		printf("%c",character); 	
+		// If statement to check if we are in a word
+		if(character == ' ' || character == '\n' || character == '\t')
+		{
+			printf("\n");
+			state = OUT;
+		}
+		// If statement to tell if we are starting or finishing a word
+		else if(state == OUT)
+		{
+			state = IN;
+			tokens++;
+		}
+	}
+	printf("\nTokens : %d\n",tokens);
+}
