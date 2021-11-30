9b5bf78e0bf5f6145704294ca79989c89c66b156
Quindarius Lyles-Woods <quinwoodz@gmail.com>
2021-11-30 16:26:58 -0500

    The file for the lexer was mishandled

--- Project/Third Deliverable/Code/lexer.c
+++ Project/Third Deliverable/Code/lexer.c
@@ -1,11 +1,32 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define IN 1 
+#define OUT 0 
+
+int main( int argc, char *argv[])
+{
+	FILE *fp;
+	int errnum; 
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		errnum = errno;
+		fprintf(stderr, "Value of : %d\n",errno);
+		perror("Error opening this file");
+		fprintf(stderr, "Error opening file: %s\n",strerror(errnum));
+	}
+	else
+	{
 	int character, 				// Gets individual character input
-	    tokens, 				// Count of tokens
+	    tokens, 				// Count of tokens 
 	    state;				// Determines if we are inside or outside a word
 	state = OUT;
-	tokens = 0;
-	while ((character = fgetc(fp)) != EOF)	//Loops through the input
+	tokens = 0; 
+	while ((character = fgetc(fp)) != EOF)	//Loops through the input		
 	{
-		printf("%c",character);
+		printf("%c",character); 	
 		// If statement to check if we are in a word
 		if(character == ' ' || character == '\n' || character == '\t')
 		{
@@ -22,4 +43,3 @@
 	printf("\nTokens : %d\n",tokens);
 	fclose(fp);
 	}}
-
