31cf44e59ee79f812015e11b1bc5d5416c427e9e
Quindarius Lyles-Woods <quinwoodz@gmail.com>
2021-11-30 16:47:15 -0500

    Was able to get the information for a structure passed along

--- Project/Third Deliverable/Code/interpreter.c
+++ Project/Third Deliverable/Code/interpreter.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+// A structure to hold the values of a symbol.
+struct symbol 			
+{
+	char word[45];
+	char type[30];
+	char type_details[30];
+	int index;
+};
 
 int main(void)
 {
-	int index;
-	int symbol-count = 12;
-	struct symbol 			// A struct to hold the valeus of a symbol
-	{
-		char word[45];
-		int index;
-		char type[30];
-		char type_details[30];
-	}
-
-struct symbols symbol[symbol-count];
-for (index = 0; index < symbol-count; index++)
-{
-	symbols[index].word = "hello world";
-}
+	/*Testing Symbol Structure*/
+	struct symbol symbol1;
+	strcpy(symbol1.word, "A word goes here");
+	strcpy(symbol1.type, "A type goes here");
+	strcpy(symbol1.type_details, "A type def goes here");
+	symbol1.index = 0;
+	
+	printf("symbol 1 information: %s", symbol1.word);
 }
 
